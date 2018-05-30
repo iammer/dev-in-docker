ad01fb565e860583652d5c54d50a3627879af026
Michael Ratliff <email@michaelratliff.com>
2018-05-29 20:30:48 -0400

    Build with node LTS

--- wetty/entrypoint.sh
+++ wetty/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "michael:$TERM_PASS" | chpasswd
 cd /srv/wetty
-NODE=$(ls -1 -d /home/michael/.nvm/versions/node/* | sort | tail -n 1)
+NODE=$(ls -1 -d /home/michael/.nvm/versions/node/v8* | sort | tail -n 1)
 exec $NODE/bin/node app.js -p 3000
