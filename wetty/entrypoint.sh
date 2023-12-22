7a6c716c40e2e4e1a350e3b066892c7ce377b89b
Michael Ratliff <email@michaelratliff.com>
2023-12-22 05:40:17 +0000

    update node and wetty

--- wetty/entrypoint.sh
+++ wetty/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "michael:$TERM_PASS" | chpasswd
-cd /srv/wetty
-NODE=$(ls -1 -d /home/michael/.nvm/versions/node/v8* | sort | tail -n 1)
-exec $NODE/bin/node app.js -p 3000
+PATH="$PATH:/opt/node/bin"
+exec wetty -c /bin/login --base /
